99a9eb8e48353559af789f60209d234c2857e8ac
Fabian Zeller <Fabian@Fabian-Laptop>
2015-03-05 14:53:00 +0100

    Felder gebaut und initialisiert, Buttons gebaut und initialisiert, fertigstellung folgt.

--- VirtuellesKeyboard/src/gui/Gui.java
+++ VirtuellesKeyboard/src/gui/Gui.java
@@ -1,18 +1,57 @@
 package gui;
 
-public class Gui {
+import java.awt.Color;
 
-	
-	/**
-	 * 
-	 */
-	
-	
-	
+import javax.swing.*;
 
-	public static void main(String[] args) {
-		System.out.println("Voll geil manu hats mal wieder geschafft!!!!");
+public class Gui extends JFrame {
+
+	JLabel label1,label2;
+	JPanel notenpane, buttonpane, tastenpane;
+		JPanel lklav,rklav;
+			JPanel lgrid,rgrid;
+				JPanel lborder,rborder;
+				
+	JToggleButton rtasten[];//Buttonanzahlt einfügen
+	JToggleButton ltasten[];
+				
+				
+	 Gui(){
+				 
+		 buttonsinitialisieren(rtasten, ltasten);
+		 buttonskonfig(rtasten, ltasten);
+		 
+				 
+		
+		
 	}
+
+	private void buttonsinitialisieren(JToggleButton rtasten[], JToggleButton ltasten[]  ) {
+			
+		for (int i = 0; i < ltasten.length; i++) {
+			ltasten[i] = new JToggleButton();
+		}
+		
+		for (int i = 0; i < rtasten.length; i++) {
+			rtasten[i] = new JToggleButton();
+		}
+			
+	}
+
+	private void buttonskonfig(JToggleButton rtasten[], JToggleButton ltasten[]  ) {
+		
+		for (int i = 0; i < ltasten.length; i++) {
+			ltasten[i].setBackground(Color.white);
+		}
+		
+		for (int i = 0; i < rtasten.length; i++) {
+			rtasten[i].setBackground(Color.white);
+		}
+	
+		
+	}
+
+
 	
 	
 	
