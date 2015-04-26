cb80d3e84874e4a84a7fc0489ffb73e39bcd8767
Fabian Zeller <Fabian@Fabian-Laptop.fritz.box>
2015-04-26 11:52:40 +0200

    Angefangen Notenlinien zu erstellen. Absprache notwendig!

--- DigitalInputComputerKeyboard/src/Gui/Notenlinien.java
+++ DigitalInputComputerKeyboard/src/Gui/Notenlinien.java
@@ -1,6 +1,8 @@
 package Gui;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,22 +16,36 @@ import javax.swing.JPanel;
 
 public class Notenlinien extends JPanel{
 
-    private BufferedImage image;
+    private BufferedImage notenschluessel;
+    private BufferedImage notenlinien;
     private JLabel lNotenschl;
+    private JPanel pNotenlinien;
 
     public Notenlinien(Gui g) {
        
+    	pNotenlinien = new JPanel();
+    	
     	try {
-    		image = ImageIO.read(getClass().getResource("/Images/notenschluessel.jpg"));
+    		notenschluessel = ImageIO.read(getClass().getResource("/Images/notenschluessel.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	
-    	lNotenschl = new JLabel(new ImageIcon(image));
+    	
+    	Image notenlinien = Toolkit.getDefaultToolkit().createImage("Background.png");
+    	
+    	lNotenschl = new JLabel(new ImageIcon(notenschluessel));
     	g.notenpane.add(lNotenschl);
+    	
     	
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+
+      super.paintComponent(g);
+          g.drawImage(notenlinien, 0, 0, null);
+  }
    
 }
